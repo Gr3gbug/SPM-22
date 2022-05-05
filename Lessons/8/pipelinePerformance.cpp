6b3f974622b6f39fc9c4e3a409a3b05ac8471ca6
Gr3gbug <l.digregoriozitel@studenti.unipi.it>
2022-05-05 22:30:20 +0200

    Lesson 8: introduced farm to speedup our bottleneck. TODO: Fix the propagation of EOS

--- Lessons/8/pipelinePerformance.cpp
+++ Lessons/8/pipelinePerformance.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <functional>
 #include <condition_variable>
+#include "../utils/utimer.cpp"
 
 #define EOS -1
 using namespace std::chrono_literals;
@@ -43,12 +44,12 @@ public:
   }
 };
 
-void drain(myqueue<int> &q, std::chrono::milliseconds msecs) {
+void drain(myqueue<int> &q) {
   std::cout << "Drain started" << std::endl;
   auto e = q.pop();
   
   while(e != EOS) {
-    std::this_thread::sleep_for(msecs);
+    std::this_thread::sleep_for(100ms);
     std::cout << "received " << e << std::endl;
     e = q.pop();
   }
@@ -56,9 +57,9 @@ void drain(myqueue<int> &q, std::chrono::milliseconds msecs) {
   return;
 }
 
-void source(myqueue<int> &q, int n, std::chrono::milliseconds msecs) {
+void source(myqueue<int> &q, int n) {
     for(int i=0; i<n; i++){
-        std::this_thread::sleep_for(msecs);
+        std::this_thread::sleep_for(100ms);
         q.push(i);
         std::cout << "Drain emitted " << i << std::endl; 
     }
@@ -67,14 +68,28 @@ void source(myqueue<int> &q, int n, std::chrono::milliseconds msecs) {
     return;
 }
 
-void genericstage(myqueue<int> &inq, myqueue<int> &outq, std::chrono::milliseconds msecs) {
+void genericstage(myqueue<int> &inq, myqueue<int> &outq) {
   auto e = inq.pop();
   
   while(e != EOS) {
-    std::this_thread::sleep_for(msecs);
-    auto res = e+10;
+    std::this_thread::sleep_for(1000ms);
+    auto res = e+1;
     outq.push(res);
     e = inq.pop();
+    std::cout << "Added " << e << std::endl;
+  }
+  outq.push(EOS);
+  return;
+}
+
+void genericstage2(myqueue<int> &inq, myqueue<int> &outq) {
+  auto e = inq.pop();
+  while(e != EOS) {
+    std::this_thread::sleep_for(2500ms);
+    auto res = e*2;
+    outq.push(res);
+    e = inq.pop();
+    std::cout << "Multiplied " << e << std::endl;
   }
   outq.push(EOS);
   return;
@@ -82,16 +97,25 @@ void genericstage(myqueue<int> &inq, myqueue<int> &outq, std::chrono::millisecon
 
 int main(int argc, char * argv[])
 {
-    myqueue<int> source2one, one2drain;
+    myqueue<int> source2one, intermediate, int2drain;
+    std::vector<std::thread> vecOfThread;
+    auto inc = [](int x) { return(x+1);}; // Dummy function
 
-    std::thread s1(source, std::ref(source2one), 10, 1000ms);
-    auto inc = [](int x) { return(x+1);};
-    std::thread s2(genericstage, std::ref(source2one), std::ref(one2drain), 1000ms);
-    std::thread s3(drain, std::ref(one2drain), 1000ms);
-
+    std::thread s1(source, std::ref(source2one), 5);
     s1.join();
+
+    std::thread s2(genericstage, std::ref(source2one), std::ref(intermediate));
     s2.join();
-    s3.join();
+
+    for (size_t i = 0; i < 2; i++)
+      vecOfThread.push_back(std::thread(genericstage2, std::ref(intermediate), std::ref(int2drain)));
+
+    for (std::thread &th : vecOfThread)
+      th.join();
+
+    std::thread s4(drain, std::ref(int2drain));
+    std::cout << "before\n";
+    s4.join();
 
     return 0;
 }
