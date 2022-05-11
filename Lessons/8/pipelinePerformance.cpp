78fc1057cad83337e146aa9fef4f021dcb55abdc
Gr3gbug <l.digregoriozitel@studenti.unipi.it>
2022-05-11 22:09:45 +0200

    Lesson 8: properly implemented farm stage

--- Lessons/8/pipelinePerformance.cpp
+++ Lessons/8/pipelinePerformance.cpp
@@ -44,14 +44,16 @@ public:
   }
 };
 
-void drain(myqueue<int> &q) {
+void drain(myqueue<int> &q, int numberOfWorkers) {
   std::cout << "Drain started" << std::endl;
   auto e = q.pop();
-  
-  while(e != EOS) {
+  int counter{0};
+  while(e != EOS && counter < numberOfWorkers) {
     std::this_thread::sleep_for(100ms);
     std::cout << "received " << e << std::endl;
     e = q.pop();
+    if(e == EOS)
+      counter++;
   }
     
   return;
@@ -68,54 +70,61 @@ void source(myqueue<int> &q, int n) {
     return;
 }
 
-void genericstage(myqueue<int> &inq, myqueue<int> &outq) {
+void stage1(myqueue<int> &inq, myqueue<int> &outq, int numberOfWorkers) {
   auto e = inq.pop();
   
   while(e != EOS) {
-    std::this_thread::sleep_for(1000ms);
+    std::this_thread::sleep_for(100ms);
     auto res = e+1;
     outq.push(res);
     e = inq.pop();
     std::cout << "Added " << e << std::endl;
   }
-  outq.push(EOS);
+  for(int nw=0; nw < numberOfWorkers; nw++)
+  {
+    outq.push(EOS); 
+  }
+
   return;
 }
 
-void genericstage2(myqueue<int> &inq, myqueue<int> &outq) {
+void farm(myqueue<int> &inq, myqueue<int> &outq, int thID) {
   auto e = inq.pop();
   while(e != EOS) {
     std::this_thread::sleep_for(2500ms);
     auto res = e*2;
     outq.push(res);
     e = inq.pop();
-    std::cout << "Multiplied " << e << std::endl;
+    std::cout << "Multiplied " << e << " from thID: " << thID <<std::endl;
   }
   outq.push(EOS);
   return;
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    myqueue<int> source2one, intermediate, int2drain;
-    std::vector<std::thread> vecOfThread;
-    auto inc = [](int x) { return(x+1);}; // Dummy function
+  int nw = atoi(argv[1]);
 
-    std::thread s1(source, std::ref(source2one), 5);
-    s1.join();
+  myqueue<int> source2one, intermediate, int2drain;
+  std::vector<std::thread> vecOfThread;
+  
+  auto inc = [](int x)
+  { return (x + 1); }; // Dummy function
 
-    std::thread s2(genericstage, std::ref(source2one), std::ref(intermediate));
-    s2.join();
+  std::thread s1(source, std::ref(source2one), 50);
+  s1.join();
 
-    for (size_t i = 0; i < 2; i++)
-      vecOfThread.push_back(std::thread(genericstage2, std::ref(intermediate), std::ref(int2drain)));
+  std::thread s2(stage1, std::ref(source2one), std::ref(intermediate), nw);
+  s2.join();
 
-    for (std::thread &th : vecOfThread)
-      th.join();
+  for (size_t i = 0; i < 2; i++)
+    vecOfThread.push_back(std::thread(farm, std::ref(intermediate), std::ref(int2drain), i));
 
-    std::thread s4(drain, std::ref(int2drain));
-    std::cout << "before\n";
-    s4.join();
+  for (std::thread &th : vecOfThread)
+    th.join();
 
-    return 0;
+  std::thread s4(drain, std::ref(int2drain), nw);
+  s4.join();
+
+  return 0;
 }
