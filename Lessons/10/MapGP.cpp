945e614d667d590373b9e8f6e5e10c3403f18a54
Gr3gbug <l.digregoriozitel@studenti.unipi.it>
2022-05-28 18:19:52 +0200

    Lesson 10: high-level programming frameworks (GrPPi, OpenMP). Composition of pattern with the frameworks.\n Hot to split data parallel map data

---
+++ Lessons/10/MapGP.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <thread>
+
+#include "grppi/pipeline.h"
+#include "grppi/map.h"
+#include "grppi/dyn/dynamic_execution.h"
+
+using namespace grppi;
+
+
+#include "utimer.cpp"
+
+using namespace std;
+
+void activewait(std::chrono::milliseconds ms) {
+  long msecs = ms.count();
+  auto start = std::chrono::high_resolution_clock::now();
+  auto end   = false;
+  while(!end) {
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+    if(msec>msecs)
+      end = true;
+  }
+  return;
+}
+
+int f(int x) {
+  activewait(10ms);
+  return(x+x);
+}
+
+int main(int argc, char * argv[]) {
+
+  if(argc == 1) {
+    cout << "Usage is:" << argv[0] << " seed n [pardegree]" << endl;
+    return(0);
+  }  
+  int seed = atoi(argv[1]);
+  int n = atoi(argv[2]);
+  int m = atoi(argv[3]);
+  int nw = atoi(argv[4]);
+
+
+  const int max = 1024;
+  srand(seed);
+  vector<int> v(n),r1(n),r2(n);
+  for(int i=0; i<n; i++)
+    v[i] = rand()%max;
+  
+
+  {
+    utimer tseq("seq");
+    transform(v.begin(), v.end(), r1.begin(),f);
+  }
+
+  
+  {
+    utimer t1("par");
+
+    grppi::pipeline(parallel_execution_native{nw},
+		    [&] () -> std::optional<vector<int>> {
+		      static int i = 0;
+		      i++;
+		      if(i<m) return {vector<int>(n,i)};
+		      else return{};
+		    },
+		    [&](vector<int> v) {
+		      grppi::map(sequential_execution{},
+				 // grppi::map(parallel_execution_native{nw},
+				 v.begin(),v.end(), v.begin(), f);
+		      return v;
+		    },
+		    [&] (vector<int> v) {
+		      cout << "Got ";
+		      for(auto &e : v)
+			cout << e << " ";
+		      cout << endl; 
+		    }
+		    );
+    
+    grppi::map(parallel_execution_native{nw},
+	       v.begin(), v.end(), r2.begin(),
+	       f);
+  }
+    
+
+  for(int i=0; i<n; i++)
+    if(r1[i] != r2[i]) 
+      cout << r1[i] << " != " << r2[i] << " at " << i << endl;
+
+  return(0);
+}
---
+++ Lessons/10/PipeG.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <optional>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace std::literals::chrono_literals;
+
+#include "grppi/pipeline.h"
+#include "grppi/farm.h"
+#include "grppi/dyn/dynamic_execution.h"
+
+using namespace grppi;
+
+void activewait(std::chrono::milliseconds ms) {
+  long msecs = ms.count();
+  auto start = std::chrono::high_resolution_clock::now();
+  auto end   = false;
+  while(!end) {
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+    if(msec>msecs)
+      end = true;
+  }
+  return;
+}
+
+#include "utimer.cpp"
+
+int inc(int i) {
+  activewait(10ms);
+  return(i+1);
+}
+
+int twice(int i) {
+  activewait(50ms);
+  return(i+i);
+}
+
+int m = 8;
+
+std::optional<int> source2() {
+  static int n = 0;
+  if(n == m)
+    return {};
+  else
+    return{n++};
+}
+
+int main(int argc, char * argv[]) {
+
+  int m;
+  int nw;
+  if (argc != 1) {
+    m=atoi(argv[1]);
+    nw = atoi(argv[2]);
+  } else {
+    m = 16; 
+    nw = 8;
+  }
+  auto source =
+    [&m] () -> std::optional<int> {
+				   static int n = 0;
+				   if(n == m)
+				     return {};
+				   else
+				     return{n++};
+  };
+
+  auto drain =
+    [] (int n) {
+      cout << "Received " << n << endl;
+    };
+
+  {
+    utimer t("pipe");
+
+    
+    // pipeline(grppi::sequential_execution{},
+
+    pipeline(grppi::parallel_execution_native{nw},
+	     source,
+	     inc,
+	     farm(2,twice),
+	     drain);
+  }
+  
+  return(0);
+  
+}
